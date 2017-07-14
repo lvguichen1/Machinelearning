2987f062d324aa627bf6a029595718d5361ed644
lvguichen <lvguichen123go@163.com>
2017-07-14 10:36:11 +0800

    庄家与赔率

--- 6.Data/6.1.Prime.py
+++ 6.Data/6.1.Prime.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-
 import numpy as np
 from time import time
 import math
@@ -17,7 +16,7 @@ def is_prime3(x):
         if p > math.sqrt(x):
             break
         if x % p == 0:
-            flag == False
+            flag = False
             break
     if flag:
         p_list2.append(x)
@@ -40,7 +39,7 @@ if __name__ == "__main__":
     print time() - t
     print p
 
-    # 方法3：利用filter和lamda
+    # 方法3：利用filter和lambda
     t = time()
     is_prime2 = (lambda x: 0 not in [x % i for i in range(2, int(math.sqrt(x)) + 1)])
     p = filter(is_prime2, range(a, b))
@@ -50,7 +49,7 @@ if __name__ == "__main__":
     # 方法4：定义
     t = time()
     p_list = []
-    for i in range(a, b):
+    for i in range(2, b):
         flag = True
         for p in p_list:
             if p > math.sqrt(i):
@@ -66,23 +65,23 @@ if __name__ == "__main__":
     # 方法5：定义和filter
     p_list2 = []
     t = time()
-    filter(is_prime3, range(a, b))
+    filter(is_prime3, range(2, b))
     print time() - t
     print p_list2
 
-    print '--------------------------'
+    print '---------------------'
     a = 750
     b = 900
     p_list2 = []
     np.set_printoptions(linewidth=150)
-    p = np.array(filter(is_prime3, range(2, b + 1)))
+    p = np.array(filter(is_prime3, range(2, b+1)))
     p = p[p >= a]
     print p
-    p_rate = float(len(p)) / float(b - a + 1)
-    print '素数的概率:', p_rate, '\t',
-    print '公正赔率：', 1 / p_rate,
-    print '合数的概率：', 1 - p_rate, '\t'
-    print '公正赔率：', 1 / (1 - p_rate),
+    p_rate = float(len(p)) / float(b-a+1)
+    print '素数的概率：', p_rate, '\t',
+    print '公正赔率：', 1/p_rate, '\t'
+    print '合数的概率：', 1-p_rate, '\t',
+    print '公正赔率：', 1 / (1-p_rate), '\t'
 
     alpha1 = 5.5 * p_rate
     alpha2 = 1.1 * (1 - p_rate)
